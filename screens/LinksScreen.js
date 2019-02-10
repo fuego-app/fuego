c510d00c29696b002392bca1f00fabd8d222c14f
huhabu <32313741+huhabu@users.noreply.github.com>
2019-02-10 11:38:58 -0800

    Update LinksScreen.js
    I added the JSON coupon info reader

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -6,17 +6,51 @@ export default class LinksScreen extends React.Component {
   static navigationOptions = {
     title: 'Links',
   };
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
+  }
 
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+  componentDidMount(){
+    return fetch('https://raw.githubusercontent.com/fuego-app/fuego/master/testCouponList.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.coupons,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+
+
+  render(){
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    return(
+      <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.title}</Text>}
+          keyExtractor={({id}, index) => id}
+        />
+      </View>
     );
   }
-}
 
 const styles = StyleSheet.create({
   container: {
