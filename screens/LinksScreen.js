c2bdce60bebf8b0e9e679695b41f888382aec5bf
Ankit Bisain <ankit.bisain@gmail.com>
2019-02-11 11:22:44 -0800

    Ø/

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -52,10 +52,5 @@ export default class LinksScreen extends React.Component {
     );
   }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
+
+}
