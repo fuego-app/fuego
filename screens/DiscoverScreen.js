967b52e56487d5d3c4ed6a4c3b60b42df599a656
paulzzy <paul.zy.zhang@gmail.com>
2019-02-18 22:36:40 -0800

    Add rudimentary button code

--- screens/DiscoverScreen.js
+++ screens/DiscoverScreen.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import { Image, TouchableOpacity, View, FlatList, ActivityIndicator, Text, ScrollView, StyleSheet } from 'react-native';
+import {
+  Image,
+  TouchableOpacity,
+  View,
+  FlatList,
+  ActivityIndicator,
+  Text,
+  ScrollView,
+  StyleSheet,
+  Buttons
+} from 'react-native';
 
 export default class DiscoverScreen extends React.Component {
   static navigationOptions = {
     title: 'Discover',
   };
 
+  constructor(props) {
+    super(props);
+  }
+
   render(){
       return(
-        <View style={styles.title}>
-            <Text>
-                Discover foobar
-            </Text>
+        <View style={styles.container}>
+          <TouchableOpacity
+            style={styles.button}
+          >
+            <Text> Company </Text>
+          </TouchableOpacity>
         </View>
-      )
+      );
   }
 }
 
 const styles = StyleSheet.create({
   title: {
-    flex: 1,
+    fontFamily: 'nunito',
+    fontSize: 20,
+  },
+  container: {
+    fontFamily: 'nunito',
+  },
+  button: {
+    fontFamily: 'nunito',
   },
 });
